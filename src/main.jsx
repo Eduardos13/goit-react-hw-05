eaffdc429ec300f7490a5b433519f9db65c7d54b
Eduardos13 <edik.kozelsky@gmail.com>
2024-07-26 19:44:00 -0400

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "modern-normalize";
-import App from "./components/App/App";
-import "src/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'modern-normalize';
+import App from './components/App/App';
+import '/src/index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </>
+  </BrowserRouter>
 );
