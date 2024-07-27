9c58737a0c2d239e356cb219dfbe8026c4550606
Eduardos13 <edik.kozelsky@gmail.com>
2024-07-26 21:20:10 -0400

    updated
    moved 2 components to home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+import s from './HomePage.module.css';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      {' '}
+      <ul className={s.buttonsList}>
+        <li className={s.buttonsItem}>
+          <NavLink to="popularofday"> Movies of the Day</NavLink>
+        </li>
+        <li className={s.buttonsItem}>
+          <NavLink to="popularofweek"> Movies of the Week</NavLink>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
+  );
 };
 
 export default HomePage;
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+
 import SearchBar from '../../components/SearchBar/SearchBar';
 import s from './MoviesPage.module.css';
 
@@ -7,15 +7,6 @@ const MoviesPage = () => {
   return (
     <div>
       <SearchBar />
-      <ul className={s.buttonsList}>
-        <li className={s.buttonsItem}>
-          <NavLink to="popularofday"> Movies of the Day</NavLink>
-        </li>
-        <li className={s.buttonsItem}>
-          <NavLink to="popularofweek"> Movies of the Week</NavLink>
-        </li>
-      </ul>
-      <Outlet />
     </div>
   );
 };
