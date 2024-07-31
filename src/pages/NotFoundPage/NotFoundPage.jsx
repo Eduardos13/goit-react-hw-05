9aaf4b1ee74e59f631e738c66fd994c5139f067c
Eduardos13 <edik.kozelsky@gmail.com>
2024-07-30 22:11:33 -0400

    done

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import s from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div>
+      <p className={s.notFound}>Sorry, page is not found...</p>
+    </div>
+  );
 };
 
 export default NotFoundPage;
